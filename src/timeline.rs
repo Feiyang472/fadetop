4fb56214b733d15ba96e4f861b76fb3d0e26ae0f
feiyang <feiyangc@outlook.com>
2025-04-15 21:07:43 +0100

    fix ci

--- src/timeline.rs
+++ src/timeline.rs
@@ -16,83 +16,79 @@ impl StatefulWidget for TimelineWidget {
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         if let Ok(queues) = state.forgetting_queues.read() {
             if let Some((_tid, queue)) = queues.iter().nth(state.selected_tab) {
-                // Render finished events
-
                 let total_duration = queue.last_update - queue.start_ts;
-
                 let (visible_end, window_width) = (
                     state.viewport_time_bound.0.unwrap_or(queue.last_update),
                     state.viewport_time_bound.1.min(total_duration),
                 );
-
                 let visible_start = visible_end - window_width;
 
                 let block = Block::default()
                     .title(
                         Line::from(format!(
-                            "-{:}:{:0>2}",
+                            "-{:0>2}:{:0>2}",
                             (queue.last_update - visible_start).as_secs() / 60,
                             (queue.last_update - visible_start).as_secs()
                         ))
                         .left_aligned(),
                     )
                     .title(
-                        Line::from(match state.viewport_time_bound.0 {
-                            Some(visible_end) => format!(
-                                "-{:}:{:0>2}",
-                                (queue.last_update - visible_end).as_secs() / 60,
-                                (queue.last_update - visible_end).as_secs()
-                            ),
-                            None => "Now".to_string(),
-                        })
+                        Line::from(state.viewport_time_bound.0.map_or(
+                            "Now".to_string(),
+                            |visible_end| {
+                                format!(
+                                    "-{:}:{:0>2}",
+                                    (queue.last_update - visible_end).as_secs() / 60,
+                                    (queue.last_update - visible_end).as_secs()
+                                )
+                            },
+                        ))
                         .right_aligned(),
                     )
                     .borders(Borders::TOP);
+
                 let inner = block.inner(area);
                 block.render(area, buf);
 
                 let width = inner.width as usize;
+
                 queue.finished_events.iter().for_each(|record| {
-                    if record.start > visible_end {
-                        return;
+                    if record.start <= visible_end
+                        && record.end >= visible_start
+                        && record.depth < inner.height as usize
+                    {
+                        render_event(
+                            buf,
+                            inner,
+                            (record.start - visible_start).as_micros() as usize,
+                            (record.end - visible_start).as_micros() as usize,
+                            record.depth as u16,
+                            &record.frame_key.name,
+                            window_width.as_micros() as usize,
+                            width,
+                            Color::Blue,
+                        );
                     }
-                    if record.end < visible_start {
-                        return;
-                    }
-                    if record.depth >= inner.height as usize {
-                        return;
-                    }
-                    render_event(
-                        buf,
-                        inner,
-                        (record.start - visible_start).as_micros() as usize,
-                        (record.end - visible_start).as_micros() as usize,
-                        record.depth as u16,
-                        &record.frame_key.name,
-                        window_width.as_micros() as usize,
-                        width,
-                        Color::Blue,
-                    );
                 });
 
-                for (depth, record) in queue
+                queue
                     .unfinished_events
                     .iter()
                     .take(inner.height as usize)
                     .enumerate()
-                {
-                    render_event(
-                        buf,
-                        inner,
-                        (record.start - visible_start).as_micros() as usize,
-                        window_width.as_micros() as usize, // Use window_width.as_micros() as usize as the "end" for unfinished events
-                        depth as u16,
-                        &record.frame_key.name,
-                        window_width.as_micros() as usize,
-                        width,
-                        Color::Red,
-                    );
-                }
+                    .for_each(|(depth, record)| {
+                        render_event(
+                            buf,
+                            inner,
+                            (record.start - visible_start).as_micros() as usize,
+                            window_width.as_micros() as usize,
+                            depth as u16,
+                            &record.frame_key.name,
+                            window_width.as_micros() as usize,
+                            width,
+                            Color::Red,
+                        );
+                    });
             }
         } else {
             state.quit();
