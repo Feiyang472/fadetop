ba8c86912e3a0df8c39aae88c4e87095bd29b544
feiyang <feiyangc@outlook.com>
2025-05-04 14:27:46 +0100

    roll threads and processes

--- src/tabs/thread_selection.rs
+++ src/tabs/thread_selection.rs
@@ -1,12 +1,13 @@
+use itertools::Itertools;
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, KeyEvent},
-    layout::Rect,
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style, Stylize},
     text::Line,
-    widgets::{Block, BorderType, Borders, Paragraph, StatefulWidget, Widget, Wrap},
+    widgets::{Block, BorderType, Borders, Paragraph, StatefulWidget, Widget},
 };
-use remoteprocess::Tid;
+use remoteprocess::{Pid, Tid};
 
 use crate::priority::{SpiedRecordQueue, SpiedRecordQueueMap, ThreadInfo};
 
@@ -14,8 +15,8 @@ use super::Blocked;
 
 #[derive(Debug, Clone, Default)]
 pub struct ThreadSelectionState {
-    selected_thread_index: usize,
-    available_threads: Vec<ThreadInfo>,
+    selected_thread_index: (usize, usize),
+    available_threads: Vec<(Pid, Vec<ThreadInfo>)>,
 }
 
 pub struct ThreadSelectionWidget {}
@@ -38,33 +39,20 @@ impl ThreadSelectionWidget {
 }
 
 impl ThreadSelectionState {
-    fn num_threads(&self) -> usize {
-        self.available_threads.len()
-    }
-
-    fn next_thread(&mut self) {
-        self.selected_thread_index = self
-            .selected_thread_index
-            .overflowing_add(1)
-            .0
-            .checked_rem(self.num_threads())
-            .unwrap_or(0);
-    }
-
-    fn prev_thread(&mut self) {
-        let num_threads = self.num_threads();
-        self.selected_thread_index = self
-            .selected_thread_index
-            .overflowing_add(num_threads.saturating_sub(1))
-            .0
-            .checked_rem(num_threads)
-            .unwrap_or(0);
-    }
-
     pub fn handle_key_event(&mut self, key: &KeyEvent) {
         match key.code {
-            event::KeyCode::Right => self.next_thread(),
-            event::KeyCode::Left => self.prev_thread(),
+            event::KeyCode::Right => {
+                self.selected_thread_index.1 = self.selected_thread_index.1.saturating_add(1)
+            }
+            event::KeyCode::Left => {
+                self.selected_thread_index.1 = self.selected_thread_index.1.saturating_sub(1)
+            }
+            event::KeyCode::Down => {
+                self.selected_thread_index.0 = self.selected_thread_index.0.saturating_add(1)
+            }
+            event::KeyCode::Up => {
+                self.selected_thread_index.0 = self.selected_thread_index.0.saturating_sub(1)
+            }
             _ => {}
         }
     }
@@ -73,7 +61,14 @@ impl ThreadSelectionState {
         &self,
         queues: &'a SpiedRecordQueueMap,
     ) -> Option<&'a SpiedRecordQueue> {
-        queues.get(&self.available_threads.get(self.selected_thread_index)?.tid)
+        queues.get(
+            &self
+                .available_threads
+                .get(self.selected_thread_index.0)?
+                .1
+                .get(self.selected_thread_index.1)?
+                .tid,
+        )
     }
 
     fn render_tabs(&mut self, area: Rect, buf: &mut Buffer) {
@@ -81,50 +76,73 @@ impl ThreadSelectionState {
             return;
         }
 
-        let titles_length = self.num_threads();
-        self.selected_thread_index = self
-            .selected_thread_index
-            .min(titles_length.saturating_sub(1));
+        let [processes_tab, threads_tab] = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Length(9), Constraint::Fill(1)])
+            .spacing(1)
+            .areas(area);
 
-        let mut lines = Vec::new();
+        let mut process_lines = Vec::new();
 
-        let mut last_pid = None;
-
-        for (i, tinfo) in self.available_threads.iter().enumerate() {
-            if Some(tinfo.pid) != last_pid {
-                lines.push(Line::from(format!("{:08x}❯", tinfo.pid)).bg(Color::Green));
+        for (i, (pid, _)) in self.available_threads.iter().enumerate() {
+            if i == self.selected_thread_index.0 {
+                process_lines.push(Line::from(format!("{:08x}❯", pid)).bg(Color::Blue));
+            } else {
+                process_lines.push(Line::from(format!("{:08x}", pid)).bg(Color::Green));
             }
-            lines.push(Line::styled(
-                match tinfo.name {
-                    Some(ref name) => format!("[{}]", name),
-                    None => format!("[{:08x}]", tinfo.tid),
-                },
-                if i == self.selected_thread_index {
-                    Style::default().bg(Color::default()).fg(Color::Blue).bold()
-                } else {
-                    Style::default()
-                },
-            ));
-            last_pid = Some(tinfo.pid);
         }
 
-        Paragraph::new(lines).render(area, buf);
+        let thread_lines = self
+            .available_threads
+            .get(self.selected_thread_index.0)
+            .map_or_else(
+                || Vec::new(),
+                |(_, thread_infos)| {
+                    thread_infos
+                        .iter()
+                        .enumerate()
+                        .map(|(j, tinfo)| {
+                            Line::styled(
+                                match tinfo.name {
+                                    Some(ref name) => format!("[{}]", name),
+                                    None => format!("[{:08x}]", tinfo.tid),
+                                },
+                                if j == self.selected_thread_index.1 {
+                                    Style::default().bg(Color::default()).fg(Color::Blue).bold()
+                                } else {
+                                    Style::default()
+                                },
+                            )
+                        })
+                        .collect()
+                },
+            );
+
+        Paragraph::new(process_lines).render(processes_tab, buf);
+        Paragraph::new(thread_lines)
+            .block(Block::new())
+            .render(threads_tab, buf);
     }
 
     pub fn update_threads(&mut self, qmaps: &SpiedRecordQueueMap) {
-        self.available_threads
-            .retain(|tinfo| qmaps.contains_key(&tinfo.tid));
-        let mut sorted_qmaps: Vec<(&Tid, &SpiedRecordQueue)> = qmaps.iter().collect();
-        sorted_qmaps.sort_by(|(_, q1), (_, q2)| q1.thread_info.pid.cmp(&q2.thread_info.pid));
-        for (tid, q) in sorted_qmaps {
-            if let None = self
-                .available_threads
-                .iter()
-                .find(|tinfo| tinfo.tid == *tid)
-            {
-                self.available_threads.push(q.thread_info.clone());
-            }
-        }
+        self.available_threads = qmaps
+            .iter()
+            .map(|(_, q)| q.thread_info.clone())
+            .into_group_map_by(|info| info.pid)
+            .into_iter()
+            .sorted_by(|(pid1, _), (pid2, _)| pid1.cmp(pid2))
+            .collect();
+
+        self.selected_thread_index = (
+            self.selected_thread_index
+                .0
+                % self.available_threads.len().max(1),
+            self.selected_thread_index.1 % (
+                self.available_threads
+                    .get(self.selected_thread_index.0)
+                    .map_or(1, |(_, threads)| threads.len().max(1))
+            ),
+        );
     }
 }
 
