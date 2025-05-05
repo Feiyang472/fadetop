cc7e08ac42bcf089f8faaf168b5483a9fc54d296
feiyang <feiyangc@outlook.com>
2025-05-05 14:40:56 +0100

    more prompts

--- src/tabs/local_variables.rs
+++ src/tabs/local_variables.rs
@@ -1,12 +1,14 @@
+
 use py_spy::stack_trace::LocalVariable;
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, KeyEvent},
     layout::{Constraint, Rect},
     style::{Color, Style, Stylize},
-    text::Line,
+    text::{Line, Span},
     widgets::{
-        Block, BorderType, Borders, HighlightSpacing, Row, StatefulWidget, Table, TableState,
+        Block, BorderType, Borders, HighlightSpacing, Paragraph, Row, StatefulWidget, Table,
+        TableState, Widget, Wrap,
     },
 };
 
@@ -17,6 +19,7 @@ use super::{StatefulWidgetExt, get_scroll};
 #[derive(Debug, Clone, Copy, Default)]
 pub struct LocalVariableSelection {
     scroll_offset: (u16, u16),
+    selected: bool,
 }
 
 impl LocalVariableSelection {
@@ -50,6 +53,7 @@ impl LocalVariableSelection {
             event::KeyCode::Down => self.move_down(),
             event::KeyCode::Left => self.move_left(),
             event::KeyCode::Right => self.move_right(),
+            event::KeyCode::Char('s') => self.selected ^= true,
             _ => {}
         }
     }
@@ -84,50 +88,77 @@ impl StatefulWidget for LocalVariableWidget<'_> {
     type State = LocalVariableSelection;
     fn render(self, local_section: Rect, buf: &mut Buffer, state: &mut Self::State) {
         if let Some(locals) = self.locals {
-            state.scroll_offset.0 = state.scroll_offset.0 % (locals.len() as u16).max(1);
-            StatefulWidget::render(
-                Table::new(
-                    locals
-                        .iter()
-                        .map(|local_var| {
-                            Row::new(vec![
-                                local_var.name.clone(),
-                                local_var.repr.clone().unwrap_or_default(),
-                            ])
-                        })
-                        .collect::<Vec<Row>>(),
-                    vec![Constraint::Fill(1), Constraint::Fill(3)],
-                )
-                .highlight_spacing(HighlightSpacing::Always)
-                .row_highlight_style(Style::new().fg(Color::LightBlue).bold()),
-                local_section,
-                buf,
-                &mut TableState::default()
-                    .with_offset(get_scroll(state.scroll_offset.0, local_section.height) as usize)
-                    .with_selected(state.scroll_offset.0 as usize),
-            );
-        }
-
-        if self.focused {
-            buf.cell_mut((
-                local_section.left() - 1,
-                local_section.top() + (state.scroll_offset.0 as u16) % local_section.height.max(1),
-            ))
-            .map(|cell| cell.set_char('↕'));
+            if state.selected {
+                if let Some(variable) = locals.get(state.scroll_offset.0 as usize) {
+                    Paragraph::new(vec![
+                        Line::from(variable.name.clone()).white().on_blue(),
+                        Line::from(variable.repr.clone().unwrap_or_default()),
+                    ])
+                    .wrap(Wrap { trim: true })
+                    .render(local_section, buf);
+                };
+            } else {
+                state.scroll_offset.0 = state.scroll_offset.0 % (locals.len() as u16).max(1);
+                StatefulWidget::render(
+                    Table::new(
+                        locals
+                            .iter()
+                            .map(|local_var| {
+                                Row::new(vec![
+                                    local_var.name.clone(),
+                                    local_var.repr.clone().unwrap_or_default(),
+                                ])
+                            })
+                            .collect::<Vec<Row>>(),
+                        vec![Constraint::Fill(1), Constraint::Fill(3)],
+                    )
+                    .highlight_spacing(HighlightSpacing::Always)
+                    .row_highlight_style(Style::new().fg(Color::LightBlue).bold()),
+                    local_section,
+                    buf,
+                    &mut TableState::default()
+                        .with_offset(
+                            get_scroll(state.scroll_offset.0, local_section.height) as usize
+                        )
+                        .with_selected(state.scroll_offset.0 as usize),
+                );
+                if self.focused {
+                    buf.cell_mut((
+                        local_section.left() - 1,
+                        local_section.top()
+                            + (state.scroll_offset.0 as u16) % local_section.height.max(1),
+                    ))
+                    .map(|cell| cell.set_char('↕'));
+                }
+            }
         }
     }
 }
 
 impl StatefulWidgetExt for LocalVariableWidget<'_> {
-    fn get_block(&self, _state: &mut Self::State) -> Block {
-        Block::default()
+    fn get_block(&self, state: &mut Self::State) -> Block {
+        let block = Block::default()
             .title(Line::from("Live Stack").bold().left_aligned())
             .borders(Borders::ALL)
-            .border_type(BorderType::Rounded)
-            .border_style(if self.focused {
-                Style::new().blue().on_dark_gray().bold().italic()
-            } else {
-                Style::default()
-            })
+            .border_type(BorderType::Rounded);
+
+        if self.focused {
+            block
+                .title_bottom(
+                    Line::from(if state.selected {
+                        vec!["de".into(), Span::from("s").underlined(), "elect".into()]
+                    } else {
+                        vec![Span::from("s").underlined(), "elect".into()]
+                    })
+                    .right_aligned(),
+                )
+                .border_style(if self.focused {
+                    Style::new().blue().on_dark_gray().bold().italic()
+                } else {
+                    Style::default()
+                })
+        } else {
+            block
+        }
     }
 }
